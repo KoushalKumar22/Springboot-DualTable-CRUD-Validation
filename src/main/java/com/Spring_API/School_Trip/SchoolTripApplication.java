f8f3f2bf1891bef45182f21385ec22d588e2c4c0
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-17 22:59:27 +0530

    Deployment

---
+++ src/main/java/com/Spring_API/School_Trip/SchoolTripApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_API.School_Trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring_API/School_Trip/SchoolTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_API.School_Trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
